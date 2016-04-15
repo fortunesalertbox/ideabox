1b1a2d9b98753bb64a13f3115a88004abc082461
fortune <fortune_ik@yahoo.com>
2016-04-15 15:50:35 +0100

    db_create.py modified

--- db_create.py
+++ db_create.py
@@ -4,7 +4,7 @@ from project.models import UserInfo, Post
 #create the db and the db tables
 db.create_all()
 
-#insert data into it
+"""#insert data into it
 db.session.add(UserInfo('fortune', 'iyke', 'legalmody@gmail.com', 'test123'))
 #db.session.add(UserInfo('ikechukwu', 'ekeruo', 'fortune_ik@yahoo.com', '123test'))
 
@@ -12,7 +12,7 @@ db.session.add(Post('First Idea Shared',
 		'This is the first Idea ever shared on Ideabox, I hope you find that cool',
 		1
 	)
-)
+)"""
 
 # commit the changes
 db.session.commit()
